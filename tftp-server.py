0178f060b830451042a2344eb77b84f00abd0f1b
richjpex <richard_pecson@dlsu.edu.ph>
2023-07-03 22:10:51 +0800

    Modified server to have default port to 69
    + Changed server to use port 69 by default
    - Has lacking features for error handling

--- tftp-server.py
+++ tftp-server.py
@@ -1,8 +1,10 @@
-import socket
-import struct
-import os
-import sys
-import time
+"""
+Richard John Pecson Jr.
+Marissa Ann Villaceran
+NSCOM01 S12
+"""
+
+import socket, struct, os, sys
 
 # TFTP packet opcodes
 OP_RRQ = 1
@@ -37,9 +39,10 @@ ERROR_MESSAGES = {
 MODE_NETASCII = "netascii"
 MODE_OCTET = "octet"
 
-# TFTP default block size, port, and timeout values
+# TFTP default block size, port number, and timeout values
 DEFAULT_BLOCK_SIZE = 512
 DEFAULT_TIMEOUT = 5
+DEFAULT_PORT = 69
 
 
 def create_packet_rrq(filename, mode):
@@ -48,11 +51,13 @@ def create_packet_rrq(filename, mode):
     """
     return struct.pack("!H", OP_RRQ) + filename.encode() + b"\x00" + mode.encode() + b"\x00"
 
+
 def create_packet_wrq(filename, mode):
     """
     Create a WRQ (write request) packet.
     """
     return struct.pack("!H", OP_WRQ) + filename.encode() + b"\x00" + mode.encode() + b"\x00"
+
 
 def create_packet_data(block_number, data):
     """
@@ -60,11 +65,13 @@ def create_packet_data(block_number, data):
     """
     return struct.pack("!HH", OP_DATA, block_number) + data
 
+
 def create_packet_ack(block_number):
     """
     Create an ACK (acknowledgment) packet.
     """
     return struct.pack("!HH", OP_ACK, block_number)
+
 
 def create_packet_error(error_code, error_message):
     """
@@ -72,87 +79,89 @@ def create_packet_error(error_code, error_message):
     """
     return struct.pack("!HH", OP_ERROR, error_code) + error_message.encode() + b"\x00"
 
+
 def parse_packet(packet):
     """
     Parse a TFTP packet and return its opcode and payload.
     """
     opcode = struct.unpack("!H", packet[:2])[0]
-    
+
     if opcode == OP_RRQ or opcode == OP_WRQ:
         parts = packet[2:].split(b"\x00")
         filename = parts[0].decode()
         mode = parts[1].decode()
         return opcode, (filename, mode)
-    
+
     elif opcode == OP_DATA:
         block_number = struct.unpack("!H", packet[2:4])[0]
         data = packet[4:]
         return opcode, (block_number, data)
-    
+
     elif opcode == OP_ACK:
         block_number = struct.unpack("!H", packet[2:4])[0]
         return opcode, block_number
-    
+
     elif opcode == OP_ERROR:
         error_code = struct.unpack("!H", packet[2:4])[0]
         error_message = packet[4:-1].decode()
         return opcode, (error_code, error_message)
-    
-def tftp_server(server_ip, server_port, root_dir=".", mode=MODE_OCTET, block_size=DEFAULT_BLOCK_SIZE, timeout=DEFAULT_TIMEOUT):
+
+
+def tftp_server(server_ip, root_dir=".", mode=MODE_OCTET, block_size=DEFAULT_BLOCK_SIZE, timeout=DEFAULT_TIMEOUT):
     """
     Run a TFTP server.
     """
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((server_ip, server_port))
-    print("Server has been created at {}:{}".format(server_ip, server_port))
-    
+    sock.bind((server_ip, DEFAULT_PORT))
+    print("Server has been created at {}:{}".format(server_ip, DEFAULT_PORT))
+
     while True:
         packet, (client_ip, client_port) = sock.recvfrom(block_size + 4)
         opcode, payload = parse_packet(packet)
-        
+
         if opcode == OP_RRQ:
             filename, mode = payload
             local_filename = os.path.join(root_dir, filename)
-            
+
             if not os.path.isfile(local_filename):
                 error_packet = create_packet_error(ERR_FILE_NOT_FOUND, ERROR_MESSAGES[ERR_FILE_NOT_FOUND])
                 sock.sendto(error_packet, (client_ip, client_port))
                 continue
-            
+
             with open(local_filename, "rb") as f:
                 block_number = 1
                 while True:
                     data = f.read(block_size)
                     data_packet = create_packet_data(block_number, data)
                     sock.sendto(data_packet, (client_ip, client_port))
-                    
+
                     try:
                         packet, _ = sock.recvfrom(block_size + 4)
                     except socket.timeout:
                         print("Timeout waiting for client response")
                         break
-                    
+
                     opcode, payload = parse_packet(packet)
-                    
+
                     if opcode == OP_ACK:
                         recv_block_number = payload
-                        
+
                         if recv_block_number == block_number:
                             block_number += 1
-                            
+
                             if len(data) < block_size:
                                 break
-                    
+
                     elif opcode == OP_ERROR:
                         error_code, error_message = payload
                         print(f"Error {error_code}: {error_message}")
                         break
-        
+
         elif opcode == OP_WRQ:
             filename, mode = payload
             local_filename = os.path.join(root_dir, filename)
-            
+
             if os.path.isfile(local_filename):
                 error_packet = create_packet_error(ERR_FILE_EXISTS, ERROR_MESSAGES[ERR_FILE_EXISTS])
                 sock.sendto(error_packet, (client_ip, client_port))
@@ -163,41 +172,41 @@ def tftp_server(server_ip, server_port, root_dir=".", mode=MODE_OCTET, block_siz
                 while True:
                     ack_packet = create_packet_ack(block_number)
                     sock.sendto(ack_packet, (client_ip, client_port))
-                    
+
                     try:
                         packet, _ = sock.recvfrom(block_size + 4)
                     except socket.timeout:
                         print("Timeout waiting for client response")
                         break
-                    
+
                     opcode, payload = parse_packet(packet)
-                    
+
                     if opcode == OP_DATA:
                         recv_block_number, data = payload
-                        
+
                         if recv_block_number == block_number + 1:
                             block_number += 1
                             f.write(data)
-                            
+
                             if len(data) < block_size:
                                 break
-                    
+
                     elif opcode == OP_ERROR:
                         error_code, error_message = payload
                         print(f"Error {error_code}: {error_message}")
                         break
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print(f"Usage: {sys.argv[0]} <server_ip> <server_port> [root_dir]")
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} <server_ip> [root_dir]")
         sys.exit(1)
-    
+
     server_ip = sys.argv[1]
-    server_port = int(sys.argv[2])
-    
-    if len(sys.argv) > 3:
-        root_dir = sys.argv[3]
+
+    if len(sys.argv) > 2:
+        root_dir = sys.argv[2]
     else:
         root_dir = "."
-    
-    tftp_server(server_ip, server_port, root_dir)
+
+    tftp_server(server_ip, root_dir)
