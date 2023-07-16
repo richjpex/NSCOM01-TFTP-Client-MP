449b3a204ff1ebedd27cd3a894eb43bdd4206b2a
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-07-17 01:25:25 +0800

    It kinda works but not really

--- NSCOM01_PECSON_VILLACERAN/tftp-client.py
+++ NSCOM01_PECSON_VILLACERAN/tftp-client.py
@@ -1,4 +1,4 @@
-import socket, struct, sys, os, shutil, subprocess
+import socket, struct, sys, os, shutil
 
 # TFTP packet opcodes
 OP_RRQ = 1
@@ -6,6 +6,11 @@ OP_WRQ = 2
 OP_DATA = 3
 OP_ACK = 4
 OP_ERROR = 5
+
+#Extension codes
+OPT_EXTENSION = 0
+OPT_BLKSIZE_CODE = 2
+OPT_TSIZE_CODE = 8
 
 # TFTP error codes
 ERR_NOT_DEFINED = 0
@@ -39,7 +44,7 @@ DEFAULT_TIMEOUT = 5
 DEFAULT_RETRY = 3
 DEFAULT_PORT = 69
 
-def create_packet_rrq(filename, mode):
+def create_packet_rrq(filename, mode, block_size=DEFAULT_BLOCK_SIZE, tsize_enabled=True, blksize_enabled=True):
     """
     Create a RRQ (read request) packet.
     Type   Op #     Format without header
@@ -53,7 +58,15 @@ def create_packet_rrq(filename, mode):
     b"\x00": null byte
 
     """
-    return struct.pack("!H", OP_RRQ) + filename.encode() + b"\x00" + mode.encode() + b"\x00"
+    
+    packet = struct.pack("!H", OP_RRQ) + filename.encode() + b"\x00" + mode.encode() + b"\x00"
+
+    if block_size != DEFAULT_BLOCK_SIZE:
+        # Append the block size option to the packet data
+        packet = append_option(packet, OPT_EXTENSION, OPT_BLKSIZE_CODE)
+        packet = append_option(packet, OPT_BLKSIZE_CODE, block_size)
+
+    return packet
 
 def create_packet_wrq(filename, mode):
     """
@@ -71,7 +84,7 @@ def create_packet_wrq(filename, mode):
     """
     return struct.pack("!H", OP_WRQ) + filename.encode() + b"\x00" + mode.encode() + b"\x00"
 
-def create_packet_data(block_number, data):
+def create_packet_data(block_number, data, block_size=DEFAULT_BLOCK_SIZE):
     """
     Create a DATA packet.
 
@@ -81,7 +94,14 @@ def create_packet_data(block_number, data):
           ---------------------------------
      data already byte because data = f.read(block_size) is in byte
     """
-    return struct.pack("!HH", OP_DATA, block_number) + data
+    if block_size != DEFAULT_BLOCK_SIZE:
+        packet = struct.pack("!HH", OP_DATA, block_number) + data
+        packet = append_option(packet, OPT_EXTENSION, struct.pack("!H", OPT_BLKSIZE_CODE))
+        packet = append_option(packet, OPT_BLKSIZE_CODE, struct.pack("!H", block_size))
+    else:
+        packet = struct.pack("!HH", OP_DATA, block_number) + data
+
+    return packet
 
 def create_packet_ack(block_number):
     """
@@ -107,24 +127,33 @@ def create_packet_error(error_code, error_message):
 
 def parse_packet(packet):
     """
-    Parse a TFTP packet and return its opcode and payload.
+    Parse a TFTP packet and return its opcode and payload, including option extension data if present.
     """
     # Unpacks the first 2 bytes of the packet
     opcode = struct.unpack("!H", packet[:2])[0]
     
     if opcode == OP_RRQ or opcode == OP_WRQ:
-        # splits packet, starting from the third byte (index 2)
+        # Splits the packet, starting from the third byte (index 2)
         parts = packet[2:].split(b"\x00")
-        # byte strings are decoded into string
+        # Byte strings are decoded into strings
         filename = parts[0].decode()
         mode = parts[1].decode()
-        return opcode, (filename, mode)
+
+        # Check if there are additional options in the packet
+        option_start = 2 + len(filename) + 1 + len(mode) + 1  # Start of options (if any)
+        if option_start < len(packet):
+            options_data = packet[option_start:]
+            options = parse_options(options_data)
+            return opcode, (filename, mode, options)
+        else:
+            return opcode, (filename, mode)
     
     elif opcode == OP_DATA:
         block_number = struct.unpack("!H", packet[2:4])[0]
-        # extracts the data content
-        # data starts from index 4 of the packet.
+        # Extracts the data content
+        # Data starts from index 4 of the packet.
         data = packet[4:]
+
         return opcode, (block_number, data)
     
     elif opcode == OP_ACK:
@@ -133,10 +162,61 @@ def parse_packet(packet):
     
     elif opcode == OP_ERROR:
         error_code = struct.unpack("!H", packet[2:4])[0]
-        # error message starts from index 4 of the packet
-        # ends at the second-to-last byte of the packet (-1 index).
+        # Error message starts from index 4 of the packet
+        # Ends at the second-to-last byte of the packet (-1 index).
         error_message = packet[4:-1].decode()
         return opcode, (error_code, error_message)
+
+def append_option(packet_data, option_code, option_value):
+    # Append the option code, option length, and option value to the packet data
+    if option_code == OPT_BLKSIZE_CODE:
+        option_length = 2  # For the blksize option, the length is always 2 bytes
+    else:
+        raise ValueError("Unsupported option code")
+
+    option_data = struct.pack("!H", option_code) + struct.pack("!H", option_length) + option_value.to_bytes(2, byteorder='big')
+    new_packet_data = packet_data + option_data
+    return new_packet_data
+
+
+
+
+
+def parse_options(options_data):
+    """
+    Parse option extension data from a TFTP packet.
+    """
+    options = {}
+    option_len = len(options_data)
+    i = 0
+
+    while i < option_len:
+        option_code = struct.unpack("!B", options_data[i:i+1])[0]
+        i += 1
+
+        option_length = struct.unpack("!B", options_data[i:i+1])[0]
+        i += 1
+
+        option_value = options_data[i:i+option_length]
+        i += option_length
+
+        options[option_code] = option_value
+
+    return options
+
+
+
+
+# Function to get user input for the block size
+def get_block_size_from_user():
+    try:
+        block_size = int(input("Enter the desired block size as an integer (default is 512): "))
+        if block_size <= 0 or block_size > 65464:
+            raise ValueError("Block size must be a positive integer between 1 and 65464.")
+        return block_size
+    except ValueError as e:
+        print("Invalid input:", e)
+        return DEFAULT_BLOCK_SIZE
 
 def tftp_client_get(server_ip, server_port, save_dir, client_filename, server_filename=None, mode=MODE_OCTET, block_size=DEFAULT_BLOCK_SIZE, timeout=DEFAULT_TIMEOUT):
     """
@@ -169,17 +249,46 @@ def tftp_client_get(server_ip, server_port, save_dir, client_filename, server_fi
     # creates a socket for IPv4 addressing (AF_INET) and using the UDP protocol (SOCK_DGRAM)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(timeout)
-    
-    # Creates RRQ packet
-    rrq_packet = create_packet_rrq(client_filename, mode)
+
+    # Creates RRQ packet with options for tsize and blksize
+    rrq_packet = create_packet_rrq(client_filename, mode, block_size=block_size)
     # sends RRQ packet
     sock.sendto(rrq_packet, (server_ip, server_port))
 
-    # Receive DATA packets and send ACK packets
-    # Saves the downloaded file to the specified save_folder
+    # Receive RRQ response packet with options
+    try:
+        packet, (server_ip, server_port) = sock.recvfrom(block_size + 4)
+    except ConnectionResetError:
+        print("Error: Failure to contact server")
+        return
+    except socket.timeout:
+        print("Timeout waiting for server response")
+        return
+
+    opcode, payload = parse_packet(packet)
+
+    # Check if the RRQ response contains options
+    if opcode == OP_RRQ and len(payload) > 2:
+        filename, mode, options_data = payload
+        options = parse_options(options_data)
+
+        # Handle the tsize option (RFC 2347)
+        if OPT_TSIZE_CODE in options:
+            tsize = int(options[OPT_TSIZE_CODE])
+            print(f"File size: {tsize} bytes")
+
+        # Handle the blksize option (RFC 2348)
+        if OPT_BLKSIZE_CODE in options:
+            server_block_size = struct.unpack("!H", options[OPT_BLKSIZE_CODE])[0]
+            print(f"Server block size: {server_block_size} bytes")
+
+            # Update block size to match the server's block size
+            block_size = server_block_size
+
+    # Save the downloaded file to the specified save_folder
     save_path = os.path.join(save_dir, server_filename)
 
-    # wb because downloading file from server to client
+    # Open the file in binary write mode
     with open(save_path, "wb") as f:
         block_number = 1
         while True:
@@ -187,21 +296,27 @@ def tftp_client_get(server_ip, server_port, save_dir, client_filename, server_fi
                 packet, (server_ip, server_port) = sock.recvfrom(block_size + 4)
             except ConnectionResetError:
                 print("Error: Failure to contact server")
+                f.close()
+                # Remove the partially downloaded file
+                os.remove(save_path)
                 return
             except socket.timeout:
                 print("Timeout waiting for server response")
+                f.close()
+                # Remove the partially downloaded file
+                os.remove(save_path)
                 return
-            
+
             opcode, payload = parse_packet(packet)
-            
+
             if opcode == OP_DATA:
                 recv_block_number, data = payload
-                
+
                 if recv_block_number == block_number:
                     f.write(data)
                     # Flush the file buffer to ensure data is written to disk
                     f.flush()
-                    free_space=shutil.disk_usage(".").free
+                    free_space = shutil.disk_usage(".").free
 
                     if free_space < block_size:
                         # creates error packet
@@ -228,6 +343,9 @@ def tftp_client_get(server_ip, server_port, save_dir, client_filename, server_fi
 
                     if not response_received:
                         print("Max retries reached. Failed to receive server response.")
+                        f.close()
+                        # Remove the partially downloaded file
+                        os.remove(save_path)
                         return
 
                     block_number += 1
@@ -239,12 +357,12 @@ def tftp_client_get(server_ip, server_port, save_dir, client_filename, server_fi
                 elif recv_block_number < block_number:
                     ack_packet = create_packet_ack(recv_block_number)
                     sock.sendto(ack_packet, (server_ip, server_port))
-            
+
             elif opcode == OP_ERROR:
                 error_code, error_message = payload
                 print(f"Error {error_code}: {error_message}")
                 f.close()
-                # removes file because corrupted
+                # Remove the partially downloaded file
                 os.remove(save_path)
                 return
 
@@ -349,13 +467,14 @@ if __name__ == "__main__":
                 server_filename = input("Server Filename: ")
                 client_filename = input("Client Filename: ")
                 save_folder = input("Which folder do you want to save it in? ")
+                block_size = get_block_size_from_user()
 
                 client_extension = get_file_extension(client_filename)
                 server_extension = get_file_extension(server_filename)
                 if client_extension != server_extension:
                     print("Error: Client and Server file names must have the same extension.")
                     continue
-                tftp_client_get(server_ip, DEFAULT_PORT, save_folder, server_filename, client_filename)
+                tftp_client_get(server_ip, DEFAULT_PORT, save_folder, server_filename, client_filename, block_size=block_size)
             
             elif command == "put":
                 file_dir = input("File directory of file to upload (Press Enter to use current directory):  ")
